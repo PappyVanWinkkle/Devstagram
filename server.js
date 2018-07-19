55ced76f686f46b958389126e6c86a66892d467a
chabz <chabz@chabzs-MacBook.local>
2018-07-19 23:46:12 +0100

    Modified server.js

--- server.js
+++ server.js
@@ -9,15 +9,8 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 // Serve static assets for production
-if (process.env.NODE_ENV === 'production') {
-	// static folder
-	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
 
-//const app = express();
+const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -39,6 +32,14 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
+
+if (process.env.NODE_ENV === 'production') {
+	// static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 
