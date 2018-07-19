8ceef6c653b2125816f4832687c575b8c9a99ca9
chabz <chabz@chabzs-MacBook.local>
2018-07-19 23:29:25 +0100

    Modified server.js

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const path = require("path")
+const path = require('path');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
+const app = express();
 // Serve static assets for production
-if (process.env.NODE_ENV === "production") {
-   // static folder 
-   app.use(express.static("client/build"))
-   app.get("*", (req, res) => {
-     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-   })
+if (process.env.NODE_ENV === 'production') {
+	// static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
-const app = express();
+//const app = express();
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,10 +27,7 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+mongoose.connect(db).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
